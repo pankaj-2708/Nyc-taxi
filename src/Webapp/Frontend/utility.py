75f2079e7ccd064e74bf421dda4f8abb309d28a1
pankaj-2708 <pankajmaulekhi32@gmail.com>
2025-08-31 14:34:20 +0530

    build_and_push_to_ecr

--- src/Webapp/Frontend/utility.py
+++ src/Webapp/Frontend/utility.py
@@ -1,6 +1,6 @@
 import requests
 
-base_url="http://127.0.0.1:8000"
+base_url="13.53.39.225:8000"
 def get_trip_duration(params_):
     return requests.request(
         url=f"{base_url}/trip_duration", method="get", json=params_
